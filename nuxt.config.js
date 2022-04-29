a52ab1427e1a8723a95cf381d70e50da5f966690
Letícia Petuconski <74278997+LePetuconski@users.noreply.github.com>
2022-04-29 11:46:45 -0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'blogYesDev',
+    title: 'Blog',
     htmlAttrs: {
       lang: 'pt-br',
     },
